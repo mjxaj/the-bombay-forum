ad17366915e3279e2bdf8d7244720daccc59571e
Unknown Entity <jasoosbo@gmail.com>
2024-07-26 18:10:49 +0530

    remove the trial return statement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,6 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    return;
     const fetchMainNewsArticle = async () => {
       try {
         const articleType = localStorage.getItem("articleType");
