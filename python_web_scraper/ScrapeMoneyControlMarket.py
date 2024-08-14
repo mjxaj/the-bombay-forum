066881ff1540708ab7c3ae9cc6d52d89d48ee95d
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2024-08-15 01:45:44 +0530

    spelling mistake fix

--- python_web_scraper/ScrapeMoneyControlMarket.py
+++ python_web_scraper/ScrapeMoneyControlMarket.py
@@ -131,7 +131,7 @@ class ScrapeMoneyControlMarket:
                             INSERT INTO news (ArticleId, Title, Description, Sphoto, Lphoto, Type,  Source, SourceLink, Link)
                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                         """
-                        values = (article_id, headings, Desc_list, smallimg, bigimg, 'Market', "NDTV", "https://www.ndtvprofit.com/technology", articleURL)
+                        values = (article_id, headings, Desc_list, smallimg, bigimg, 'Markets', "NDTV", "https://www.ndtvprofit.com/technology", articleURL)
                         cursor.execute(insert_query, values)
                     else:
                         print(f"Article with ArticleId {article_id} already exists, skipping.")
