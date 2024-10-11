da56359b472e91e5c3cb0de12ae70b197536c9df
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2024-10-11 22:14:13 +0530

    error logging

--- src/app/api/admin/news/route.ts
+++ src/app/api/admin/news/route.ts
@@ -50,6 +50,6 @@ export async function POST(request: Request) {
     return NextResponse.json({ message: "News added successfully" });
   } catch (error) {
     console.error("Error adding news:", error);
-    return NextResponse.json({ error: "Failed to add news" }, { status: 500 });
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
